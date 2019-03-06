6e9fa914c4b51fc06097496d2348347f025bb79f
Manuel García García <marianocucufate@gmail.com>
2019-03-06 10:52:33 +0100

    added tree

--- 0_apt-get/add_package.bash
+++ 0_apt-get/add_package.bash
@@ -6,9 +6,10 @@ new_package=$1
 if [ -z ${new_package} ]
 then
     echo "usage: $0 <package name>"
+    exit
 fi
 
-sudo apt-get install ${new_package}
+sudo apt-get install -y ${new_package}
 
 cd ${working_dir}
 echo ${new_package} >> packages.txt

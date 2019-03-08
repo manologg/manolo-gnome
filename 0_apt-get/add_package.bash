b343c641fbfcbc4527013d385530f34a973999ca
Manuel García García <marianocucufate@gmail.com>
2019-03-08 19:22:10 +0100

    added comments

--- 0_apt-get/add_package.bash
+++ 0_apt-get/add_package.bash
@@ -3,14 +3,17 @@
 working_dir=`dirname $0`
 new_package=$1
 
+# check args
 if [ -z ${new_package} ]
 then
     echo "usage: $0 <package name>"
     exit
 fi
 
+# install
 sudo apt-get install -y ${new_package}
 
+# add to git
 cd ${working_dir}
 echo ${new_package} >> packages.txt
 git add -A

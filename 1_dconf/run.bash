21d92353eff5dc5f6520d5efff77667a122f00af
Manuel García García <manuel.garcia.garcia@zalando.de>
2019-09-02 22:05:47 +0200

    CLOSES #1 added $(whoami) in dconf scripts

--- 1_dconf/run.bash
+++ 1_dconf/run.bash
@@ -3,5 +3,5 @@
 working_dir=`dirname $0`
 
 cp ${working_dir}/debian.jpg ~/
-cat ${working_dir}/dconf.conf | dconf load /
+cat ${working_dir}/dconf.conf | sed 's/{whoami}/'$(whoami)'/g' | dconf load /
 

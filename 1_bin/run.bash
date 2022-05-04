6b3e4627fb043bf07c6bffe33608a4ef25b2f9d6
Manuel García García <marianocucufate@gmail.com>
2022-05-04 14:10:35 +0200

    download all git repos

--- 1_bin/run.bash
+++ 1_bin/run.bash
@@ -4,7 +4,7 @@ working_dir=`dirname $0`
 
 for file in mgg@foryouandyourcustomers.com whereby-manuelgarciagarcia
 do
-    sudo cp $file /usr/bin/
+    sudo cp ${working_dir}/${file} /usr/bin/
     sudo chmod +x /usr/bin/$file
 done
 
--- 4_git/run.bash
+++ 4_git/run.bash
@@ -7,3 +7,16 @@ git config --global user.name "Manuel García García"
 # tools
 git config --global core.editor vim
 git config --global merge.tool meld
+
+mkdir -p ~/private_git
+cd ~/private_git
+
+for root in "users/manologg" "orgs/thunder-guanaco" "orgs/saperia-core"
+do
+    curl -s "https://api.github.com/${root}/repos?per_page=100" | jq '.[].ssh_url' | xargs -I{} git clone {}
+done
+
+
+git clone git@github.com:saperia-core/reply-code-challenge-2021.git
+git clone git@github.com:saperia-core/mekami.git
+
